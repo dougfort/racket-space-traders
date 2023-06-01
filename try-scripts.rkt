dffbd2b891d26ba6db3484b5a08fec22f47dd5db
dougfort <doug.fort@gmail.com>
2023-06-01 16:38:23 -0400

    Clean things up by adding some structs.
    It's still pretty obscure.

--- try-scripts.rkt
+++ try-scripts.rkt
@@ -10,10 +10,17 @@
 (define (current-utc-date)
   (seconds->date (current-seconds) #f))
 
-(define (make-state script-id)
-  (hash 'script-id script-id 'script-index 0))
+(define (make-state)
+  (hash))
 
-(struct task-result (timestamp state))
+(struct task (label fn))
+(struct script-pos (id index))
+(struct script-next (script-id op))
+(struct task-step (pos state)) 
+(struct task-result (timestamp next state))
+
+(define navigate-script-id 'navigate-script)
+
 
 ;; navigate to agent headquarters
 ;; then navige to the asteroid field and back
@@ -23,43 +30,43 @@
   (define asteroid-field "X1-NU19-72345Z")
   (define hq (agent-headquarters))
   (define max-count 3)
-  
+  (define navigate-to-hq
+    (λ (script-id state) (let ([timestamp (navigate ship-id hq)])
+                           (task-result timestamp (script-next script-id 'increment) state)))) 
+  (define navigate-to-asteroid-field
+    (λ (script-id state) (let ([timestamp (navigate ship-id asteroid-field)])
+                           (task-result timestamp (script-next script-id 'increment) state)))) 
+  (define check-count
+    (λ (script-id state) (let ([timestamp (current-utc-date)]
+                               [next-count (add1 (hash-ref state 'count 0))])
+                           (cond                                
+                             [(>= next-count max-count)
+                              (task-result timestamp
+                                           (script-next script-id 'increment)
+                                           (hash-set state 'count 0))]
+                             [else
+                              (task-result timestamp
+                                           (script-next script-id 'repeat)
+                                           (hash-set state 'count next-count))]))))
+    
+     
   (list->vector (list
-                 (λ (state) (let* ([timestamp (navigate ship-id hq)]
-                                   [next-index (add1 (hash-ref state 'script-index))]
-                                   [next-state (hash-set state 'script-index next-index)])
-                               (task-result timestamp next-state)))
-                 (λ (state) (let ([timestamp (navigate ship-id asteroid-field)]
-                                   [next-index (add1 (hash-ref state 'script-index))])
-                               (task-result timestamp (hash-set state 'script-index next-index))))
-                 (λ (state) (let ([timestamp (navigate ship-id hq)]
-                                   [next-index (add1 (hash-ref state 'script-index))])
-                               (task-result timestamp (hash-set state 'script-index next-index))))
-                 (λ (state) (let* ([timestamp (current-utc-date)]
-                                    [count (hash-ref state 'count 0)]
-                                    [next-index (cond                                
-                                                  [(>= count max-count)
-                                                   (add1 (hash-ref state 'script-index))]
-                                                  [else
-                                                   (- (hash-ref state 'script-index) 2)])]
-                                    [next-count (cond                                
-                                                  [(>= count max-count) 0]
-                                                  [else (add1 count)])])
-                               (task-result timestamp (hash-set* state
-                                                                 'script-index next-index
-                                                                 'count next-count)))))))
-                                  
-                                      
+                 (task null navigate-to-hq)
+                 (task 'repeat navigate-to-asteroid-field)
+                 (task null navigate-to-hq)
+                 (task null check-count))))
+                                                                        
 
 (define (run)
   (define queue (make-queue))
   (define scripts (make-hash))
-  (define navigate-script-id 'navigate-script)
   
 
   (hash-set! scripts navigate-script-id (build-navigate-script))
   
-  (queue-push-by-date! queue (current-utc-date) (make-state navigate-script-id))
+  (queue-push-by-date! queue
+                       (current-utc-date)
+                       (task-step (script-pos navigate-script-id 0) (make-state)))
 
   (process-queue scripts queue))
 
@@ -68,9 +75,11 @@
     [(queue-empty? queue) (println "queue is empty")]
     [else
      (queue-wait queue)
-     (let* ([state (queue-pop queue)]
-            [script-id (hash-ref state 'script-id)]
-            [script-index (hash-ref state 'script-index)]
+     (let* ([step (queue-pop queue)]
+            [pos (task-step-pos step)]
+            [state (task-step-state step)]
+            [script-id (script-pos-id pos)]
+            [script-index (script-pos-index pos)]
             [script (hash-ref scripts script-id)])
        (printf "script-id ~s; script-index ~s; count ~s~n"
                script-id script-index (hash-ref state 'count 0))
@@ -78,13 +87,31 @@
          [(>= script-index (vector-length script))
           (printf "end of script ~s~n" script-id)]
          [else
-          (let ([fn (vector-ref script script-index)])
-            (let ([result (fn state)])
+          (let* ([task-item (vector-ref script script-index)]
+                 [fn (task-fn task-item)])
+            (let* ([result (fn script-id state)]
+                   [timestamp (task-result-timestamp result)]
+                   [next (task-result-next result)]
+                   [state (task-result-state result)]
+                   [script-id (script-next-script-id next)]
+                   [op (script-next-op next)])
+              (let ([index (cond
+                             [(equal? op 'increment) (add1 script-index)]
+                             [else (find-label-index script op)])])
+                                                
               (queue-push-by-date! queue
-                                   (task-result-timestamp result)
-                                   (task-result-state result))))])
-          (process-queue scripts queue))]))
+                                   timestamp
+                                   (task-step (script-pos script-id index) state)))))])
+       (process-queue scripts queue))]))
 
+;; search a script vector
+;; returning the index of a matching label
+(define (find-label-index script label)
+  (for/or ([i (in-naturals)])
+    (let ([task-item (vector-ref script i)])
+      (if (equal? (task-label task-item) label)
+          i
+          #f))))
             
 ;; navigate the ship to the specified location
 ;; do nothing if the ship is already there
