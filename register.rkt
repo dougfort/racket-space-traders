3f70f91bedc69c755e469c470ad194d0cd36489f
dougfort <doug.fort@gmail.com>
2023-06-24 13:16:57 -0400

    registration: write access token to disk

--- register.rkt
+++ register.rkt
@@ -4,21 +4,9 @@
 
 (require net/http-client)
 (require json)
+(require threading)
 
-(define callsign "DRFOGOUT")
-;(define faction "COSMIC")
-(define faction "QUANTUM")
-(define available-factions '("COSMIC"
-                             "VOID"
-                             "GALACTIC"
-                             "QUANTUM"
-                             "DOMINION"
-                             "ASTRO"
-                             "CORSAIRS"
-                             "OBSIDIAN"
-                             "AEGIS"
-                             "UNITED"))
-(define email "doug.fort@gmail.com")
+(define token-file "access-token.txt")
 (define expected-status 201)
 
 ;; looking for '("HTTP/1.1" "200" "OK")
@@ -27,7 +15,7 @@
   (let ([parts ((compose1 string-split bytes->string/utf-8) status-line)])
     (values ((compose string->number second) parts) (string-join (drop parts 2)))))
 
-(define (register-agent)
+(define (register-new-agent callsign faction email)
   (let ([uri "/v2/register"]
         [post-data (jsexpr->string (hash 'symbol callsign 'faction faction 'email email))])
     (let-values ([(status-line header-list data-port)
@@ -43,3 +31,30 @@
           (cond
             [(= expected-status status) body]
             [else (error (format "invalid HTTP status ~s; ~s; ~s; ~s" status reason uri body))]))))))
+
+(define (register)
+  (define callsign "DRFOGOUT")
+  ;(define faction "COSMIC")
+  (define faction "QUANTUM")
+  (define available-factions '("COSMIC"
+                               "VOID"
+                               "GALACTIC"
+                               "QUANTUM"
+                               "DOMINION"
+                               "ASTRO"
+                               "CORSAIRS"
+                               "OBSIDIAN"
+                               "AEGIS"
+                               "UNITED"))
+  (define email "doug.fort@gmail.com")
+
+  (let* ([result (register-new-agent callsign faction email)]
+         [token (~> result
+                    (hash-ref 'data)
+                    (hash-ref 'token))])
+    (println token)
+    (with-output-to-file token-file
+      (λ () (printf token)))))
+    
+         
+    
