a6f80d5c890927e9c01e0e0bdad554d8094a459b
dougfort <doug.fort@gmail.com>
2023-07-01 16:53:40 -0400

    Move api/ files out to space-traders-v2

--- Main.rkt
+++ Main.rkt
@@ -2,3 +2,12 @@
 
 ;; 
 
+(require space-traders-v2/http)
+(require space-traders-v2/fleet)
+
+(define (read-access-token)
+  (define access-token-path "access-token.txt")
+  (string-trim (port->string (open-input-file access-token-path) #:close? #t)))
+
+(parameterize ([access-token (read-access-token)])
+  (list-ships))
